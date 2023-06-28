07951bcec6e0b4a748c4e2690bb079b0de98f18e
Dave Pham <dave.pham@legato.co>
2023-06-28 21:20:04 +0700

    add terraform

---
+++ infrastructure/terraform/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">= 0.12"
+
+    required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.5"
+    }
+  }
+}
+
+variable "access_key" {}
+variable "secret_key" {}
+
+provider "aws" {
+  region = "ap-southeast-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "aws_instance" "intern-devops" {
+  ami           = "ami-0df7a207adb9748c7"
+  instance_type = "t2.micro"
+  security_groups = ["launch-wizard-4"]
+
+  tags = {
+    Name = "intern-devops"
+  }
+}
