2031677d08ee4713f9c76604a9704bd0016b973a
test <test@yahoo.com>
2023-07-27 00:59:34 +0700

    update dockerfile and testing some bash scripts

---
+++ infrastructure/bash/install-aws.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
+rm -rf aws 
+rm -f awscliv2.zip
+aws configure set
