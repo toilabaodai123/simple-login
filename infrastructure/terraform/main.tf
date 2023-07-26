066e215ffeeecca15aeee484468e5b1751522989
test <test@yahoo.com>
2023-07-26 16:35:14 +0700

    update infrastructure

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -9,6 +9,10 @@ terraform {
   }
 }
 
+variable "aws_ec2_web_app_ami" {}
+variable "aws_ec2_jenkins_ami" {}
+variable "aws_region" {}
+variable "aws_ec2_user" {}
 variable "access_key" {}
 variable "secret_key" {}
 variable "ansible_working_dir" {}
@@ -16,33 +20,33 @@ variable "ec2_ssh_private_key" {}
 variable "jenkins_ssh_private_key" {}
 
 provider "aws" {
-  region = "ap-southeast-1"
+  region = var.aws_region
   access_key = var.access_key
   secret_key = var.secret_key
 }
 
-resource "aws_instance" "intern-devops" {
-  ami           = "ami-0df7a207adb9748c7"
-  instance_type = "t2.micro"
+# resource "aws_instance" "intern-devops" {
+#   ami           = "ami-0df7a207adb9748c7"
+#   instance_type = "t2.micro"
 
-  key_name = "web-server"
-  security_groups = ["launch-wizard-4"]
+#   key_name = "web-server"
+#   security_groups = ["launch-wizard-4"]
 
-  tags = {
-    Name = "intern-devops"
-  }
+#   tags = {
+#     Name = "intern-devops"
+#   }
 
-  provisioner "local-exec" {
-    working_dir = var.ansible_working_dir
-    command = "ansible-playbook --inventory ${self.public_ip}, --private-key ${var.ec2_ssh_private_key} --user ubuntu config-new-ec2-instance.yaml"
-  }
-}
+#   provisioner "local-exec" {
+#     working_dir = var.ansible_working_dir
+#     command = "ansible-playbook --inventory ${self.public_ip}, --private-key ${var.ec2_ssh_private_key} --user ${var.aws_ec2_user} config-new-ec2-instance.yaml"
+#   }
+# }
 
 resource "aws_instance" "jenkins" {
-  ami           = "ami-0df7a207adb9748c7"
+  ami           = var.aws_ec2_jenkins_ami
   instance_type = "t2.micro"
 
-  security_groups = ["launch-wizard-4"]
+  security_groups = ["Jenkins"]
   key_name = "jenkins"
 
   tags = {
@@ -51,7 +55,7 @@ resource "aws_instance" "jenkins" {
 
   provisioner "local-exec" {
     working_dir = var.ansible_working_dir
-    command = "ansible-playbook --inventory ${self.public_ip}, --private-key ${var.jenkins_ssh_private_key} --user ubuntu config-jenkins-instance.yaml"
+    command = "ansible-playbook --inventory ${self.public_ip}, --private-key ${var.jenkins_ssh_private_key} --user ${var.aws_ec2_user} config-jenkins-instance.yaml"
   }
 }
 
