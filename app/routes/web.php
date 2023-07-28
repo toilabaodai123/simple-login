c7088529d0a2a7493fc7fa976400ba55425c0f5b
test <test@yahoo.com>
2023-07-29 00:54:18 +0700

    update

--- app/routes/web.php
+++ app/routes/web.php
@@ -16,11 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/test', function () {
-    return "new branch 2";
-});
-
-Route::get('/new-branch', function () {
-    return "testing for ci/cd";
-});
